7336b63f15f5631a2f077290db02360d08bc51f2
ian-codes <ian.kraft@hotmail.com>
2024-01-15 21:45:02 +0100

    initial commit

---
+++ src/lib/Menu.svelte
@@ -0,0 +1,33 @@
+
+
+<div class="container">
+    <button class="menu-button">
+        Menu
+    </button>
+</div>
+
+
+
+<style>
+    .container {
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .menu-button {
+        font-size: 1em;
+        text-transform: uppercase;
+        cursor: pointer;
+        background: white;
+        padding: 1em;
+    }
+
+    .menu-button:hover {
+        filter: invert(1);
+    }
+</style>
