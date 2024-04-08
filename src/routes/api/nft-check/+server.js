f11c46c37d3ad873e5b18ffa568bafeec51340f4
ian-codes <ian.kraft@hotmail.com>
2024-04-08 16:23:50 +0200

    try fix base path error

--- src/routes/api/nft-check/+server.js
+++ src/routes/api/nft-check/+server.js
@@ -1,12 +1,12 @@
-import { BLOCKFROST_API_KEY } from '$env/static/private';
+// import { BLOCKFROST_API_KEY } from '$env/static/private';
 
 
-export function GET(Request) {
-    // https://cardano-mainnet.blockfrost.io/api/v0
-    // TODO: implement check NFT in address method
-    return new Response(BLOCKFROST_API_KEY);
-}
+// export function GET(Request) {
+//     // https://cardano-mainnet.blockfrost.io/api/v0
+//     // TODO: implement check NFT in address method
+//     return new Response(BLOCKFROST_API_KEY);
+// }
 
-export const config = {
-    runtime: 'edge',
-};
+// export const config = {
+//     runtime: 'edge',
+// };
